f51117fdb36646c5776c8961932074bf7284be07
Maarten Schmeitz <info@scns.nl>
2025-08-17 20:44:29 +0200

    Update HTML dashboard title for multitenant clarity

--- Get-PIMUsers.ps1
+++ Get-PIMUsers.ps1
@@ -1319,7 +1319,7 @@ function New-HTMLDashboard {
 <html lang="nl">
 <head>
     <meta charset="UTF-8">
-    <title>PIM & Role Assignment Dashboard</title>
+    <title>PIM & Role Assignment Dashboard Multitenant</title>
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
